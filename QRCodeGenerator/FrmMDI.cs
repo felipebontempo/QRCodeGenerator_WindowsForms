57a0a97f997afe4c99966ee06f2fec3452bd97a2
Felipe Bontempo <felipe.bontempo@gmail.com>
2021-06-09 14:51:23 -0300

    adicionado imagens como recursos e novo form como principal
    - efetuei alguns ajustes nesse form FrmMDI, como alteração de bortão e coloquei ele como form MIDI.
    - o form1 criado junto com o projeto foi deletado.

--- QRCodeGenerator/FrmMDI.cs
+++ QRCodeGenerator/FrmMDI.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace QRCodeGenerator
 {
-    public partial class Form1 : Form
+    public partial class FrmMDI : Form
     {
-        public Form1()
+        public FrmMDI()
         {
             InitializeComponent();
         }
