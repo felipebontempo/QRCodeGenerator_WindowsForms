5640e80b601de1818f8c979a4d589a16a01cdcda
Felipe Bontempo <felipe.bontempo@gmail.com>
2021-07-13 14:39:10 -0300

    Formulário para Gerar QRCode do Tipo URL - Parte 03
    - Ação do botão salvar qrcode
    - Criado uma classe padrão de salvar imagem 'SalveImage.cs'

--- QRCodeGenerator/Forms/Frm_QRCode_URL.cs
+++ QRCodeGenerator/Forms/Frm_QRCode_URL.cs
@@ -12,9 +12,12 @@ namespace QRCodeGenerator.Forms
 {
     public partial class Frm_QRCode_URL : Form
     {
+        private Image _imgQrcode;
+        private string _format;
         public Frm_QRCode_URL()
         {
             InitializeComponent();
+            Btn_GenerateClean_QRCode_URL_02_Click(Btn_GenerateClean_QRCode_URL_02, new EventArgs());
         }
 
         private void Btn_Generate_QRCode_URL_01_Click(object sender, EventArgs e)
@@ -24,14 +27,17 @@ namespace QRCodeGenerator.Forms
 
         private void Btn_GenerateClean_QRCode_URL_02_Click(object sender, EventArgs e)
         {
+            _format = string.Empty;
+            _imgQrcode = null;
             Txt_URL.Text = string.Empty;
             Pic_Image_QRCodeURL.BackgroundImage = null;
-            /*O botão Clean, para limpar formulário não funciona, porque estou tentando chamar uma função em um form User Control, não consegui fazer funcionar, por isso criei uma botão a parte.*/          
+            /*O botão Clean, para limpar formulário não funciona, porque estou tentando chamar uma função em um form User Control, não consegui fazer funcionar, por isso criei uma botão a parte.*/
+            //UC_QRConfig.SetDefaultOptions(); ta dando ruim
         }
 
         private void Btn_GenerateSave_QRCode_URL_03_Click(object sender, EventArgs e)
         {
-
+            SaveImage.Save(_imgQrcode, _format);
         }
     }
 }
---
+++ QRCodeGenerator/SaveImage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QRCodeGenerator
+{
+    public static class SaveImage
+    {
+        public static void Save(Image imgQRCode, string format)
+        {
+            SaveFileDialog sfDialog = new SaveFileDialog();//Abre a janela para salvar arquivos, tipo DialogModal
+
+            //verifica o formato da imagem
+            if (format.Equals("png"))
+            {
+                sfDialog.Filter = "PNG Image|*.png";//o formato precisa ser assim mesmo '"PNG Image|*.png"'
+            }
+            else if (format.Equals("gif"))
+            {
+                sfDialog.Filter = "GIF Image|*.gif";
+            }
+            else if (format.Equals("jpeg"))
+            {
+                sfDialog.Filter = "JPEG Image|*.jpeg";
+            }
+            else //JPG
+            {
+                sfDialog.Filter = "JPG Image|*.jpg";
+            }
+            /*o método '.Filter', é para mostrar somente os tipos de arquivos que estamos definindo.*/
+
+            sfDialog.Title = "Save QRCode"; //nome da janela
+            sfDialog.FileName = "QRCode"; //nome padrão do arquivo
+            sfDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); //Ao abrir a janela a mesma será aberta por padrão no desktop do usuário.
+            sfDialog.ShowDialog(); //Exiba a janela!
+        }
+    }
+}
